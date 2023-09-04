cc3a1eee3bd6d0ef11562436cfd52bc3b0640235
Tommy2517 <artem07stu@gmail.com>
2023-09-04 12:15:57 +0300

    lesson_1_part_2-not_done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,52 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './components/app.component';
-import { UsersComponent } from './components/users/users.component';
-import { UserComponent } from './components/user/user.component';
-import { UserDetailsComponent } from './components/user-details/user-details.component';
+import {AppComponent} from './components/app.component';
+import {UsersComponent} from './components/users/users.component';
+import {UserComponent} from './components/user/user.component';
+import {UserDetailsComponent} from './components/user-details/user-details.component';
 import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+import {HeaderComponent} from './components/header/header.component';
+import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
+import {UserDetailPageComponent} from './pages/user-detail-page/user-detail-page.component';
+import {UsersPageComponent} from './pages/users-page/users-page.component';
+
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {
+        path: '', redirectTo: 'users', pathMatch: 'full'
+      },
+      {
+        path: 'users', component: UsersPageComponent, children: [
+          {
+            path: ':id', component: UserDetailPageComponent
+          }
+        ]
+      }
+    ]
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     UserComponent,
-    UserDetailsComponent
+    UserDetailsComponent,
+    HeaderComponent,
+    MainLayoutComponent,
+    UserDetailPageComponent,
+    UsersPageComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
