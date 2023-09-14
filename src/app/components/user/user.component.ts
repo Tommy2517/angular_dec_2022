b5631f146b51861485ec0050ac786edad87e1502
Tommy2517 <artem07stu@gmail.com>
2023-09-14 16:13:44 +0300

    lesson_1_part_2-done!!

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, Input,} from '@angular/core';
 import {IUser} from "../interfaces";
 import {ActivatedRoute, Router} from "@angular/router";
 
